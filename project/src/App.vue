10d0b7384b531f28c752f2678d56d7b2caecd062
MajaBrezjan <72605162+MajaBrezjan@users.noreply.github.com>
2020-10-30 15:01:14 +0100

    latest

---
+++ project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <Index/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar.vue'
+import Index from './components/Index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Index
+  }
+}
+</script>
+
+<style>
+
+</style>
