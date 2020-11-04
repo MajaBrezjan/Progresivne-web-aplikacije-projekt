6ef5f62c958b8d6ae03d71b5cf509da3cf9ec7e2
MajaBrezjan <72605162+MajaBrezjan@users.noreply.github.com>
2020-11-04 17:29:03 +0100

    update add subject

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">     
+    <Navbar/>
+    <Index/>
+    <AddSubject/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from './components/Navbar.vue'
+import Index from './components/Index.vue'
+import AddSubject from './components/AddSubject.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Index,
+    AddSubject
+  }
+}
+</script>
+
+<style>
+
+</style>
